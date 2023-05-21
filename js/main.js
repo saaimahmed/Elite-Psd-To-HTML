0df146f5021a75e8580961a55d349e42dce981e6
saaim ahmed <96498899+saaimahmed@users.noreply.github.com>
2023-05-22 01:17:50 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+(function ($) {
+
+
+
+    // Causes progress
+    $('.causes-progress').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1000,
+        center: true,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+
+
+})(jQuery);
+
+
+
+
+
+
